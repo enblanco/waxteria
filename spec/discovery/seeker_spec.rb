f20666515a7cf7e065157f2e6000fdf53d258d3c
Alvaro Bautista <alvarobp@gmail.com>
2013-07-20 17:34:03 +0200

    Use rspec expect syntax consistently

--- spec/discovery/seeker_spec.rb
+++ spec/discovery/seeker_spec.rb
@@ -6,7 +6,7 @@ module Discovery
       it "retrieves items matching given terms" do
         items = [Item.new(:title => 'Wimple Winch Save My Soul 45 Single'), Item.new(:title => 'Wimple Winch Collection')]
         seek_adapter = double(:seek_adapter)
-        seek_adapter.stub(:seek).with('wimple winch').and_return(items)
+        allow(seek_adapter).to receive(:seek).with('wimple winch').and_return(items)
         seeker = Seeker.new(seek_adapter)
         seeker.seek('wimple winch').should == items
       end
